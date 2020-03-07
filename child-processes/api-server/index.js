7af7fb49af111a59c804fe0be12a77ba213571ec
Omid Navy <omidnavy@yahoo.com>
2020-03-07 11:16:19 +0330

    comment fixed

--- child-processes/api-server/index.js
+++ child-processes/api-server/index.js
@@ -1,4 +1,4 @@
-//if the child process killed by "SIGKILL" , close this child process also.
+//if the parent process killed by "SIGKILL" , close this child process also.
 //this is to make sure no zombie process remains
 process.on('disconnect', () => process.exit());
 //I like to use our "request" function as a global object,
